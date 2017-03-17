73adba343dbd5e8eed357fd1f9d93afe43b735ba
iliass <iliass24@gmail.com>
2017-03-17 16:30:05 -0400

    add build script to make a bundle file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const webpack = require('webpack');
 const path = require('path');
-const config = require('../webpack.config.dev');
-const open = require('open');
 const models = require('./models');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -12,33 +9,6 @@ const router = require('./routes');
 
 const port = process.env.PORT || 2017;
 const app = express();
-const compiler = webpack(config);
-
-printErrors =(summary, errors)=> {
-	console.log(summary);
-	errors.forEach(err => {
-		console.log(err.message || err);
-	});
-};
-compiler.run((err, stats) => {
-	if (err) {
-		printErrors('Failed to compile.', [err]);
-		process.exit(1);
-	}
-
-	if (stats.compilation.errors.length) {
-		printErrors('Failed to compile.', stats.compilation.errors);
-		process.exit(1);
-	}
-
-	if (process.env.CI && stats.compilation.warnings.length) {
-		printErrors(
-			'Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.',
-			stats.compilation.warnings
-		);
-		process.exit(1);
-	}
-});
 
 //body-parser middleware adds .body property to req (if we make a POST AJAX request with some data attached, that data will be accessible as req.body)
 app.use(bodyParser.urlencoded({extended: true}));
@@ -49,14 +19,6 @@ app.get('/image/:img', (req, res) => {
 	let img = req.params.img;
   res.sendFile(path.join(__dirname, `images/${img}`));
 });
-
-app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true,
-	publicPath: config.output.publicPath
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
 
 models.sequelize.sync().then(() => {
 	/**
@@ -76,6 +38,9 @@ if(!module.parent){
 });
 
 app.use("/api", router);
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/index.html'));
